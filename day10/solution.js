a35af98ce7e6157fde2479197f57817522a972ca
Jan Rudolf <jan.rudolf@plus4u.net>
2022-01-18 17:01:18 +0100

    Day 10 - solved, but a little "heuristically", see COMBO_MAP constant

--- day10/solution.js
+++ day10/solution.js
@@ -15,5 +15,34 @@ sortedAdapters.forEach((value, index) => {
 
 console.log("-> partOne", diffs.get(1) * diffs.get(3));
 
-// TODO part two
+// based on input data, the differences are always 1 or 3, so it means that it is always
+// list of consecutive numbers, then a difference by 3 and then a list of another consecutive numbers
 
+// this constant was done manually, I was not able to think about some logic into it
+const COMBO_MAP = {
+  1: 1,
+  2: 1,
+  3: 2,
+  4: 4,
+  5: 7
+};
+
+let variants = 1;
+let currentIndex = 0;
+while (currentIndex < sortedAdapters.length) {
+  let currentNum = sortedAdapters[currentIndex];
+
+  for (let i = 1; i < sortedAdapters.length; i++) {
+    if (currentNum + i !== sortedAdapters[currentIndex + i]) {
+      // "i" is count of consecutive numbers, so we can calculate based on this probably
+      let comboVariants = COMBO_MAP[i.toString()];
+
+      if (!comboVariants) throw "Should not happen, based on heuristics lol";
+
+      variants *= comboVariants;
+      currentIndex += i;
+      break;
+    }
+  }
+}
+console.log("-> partTwo", variants);
