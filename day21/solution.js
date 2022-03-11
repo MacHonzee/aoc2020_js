c953bf9df3f89a0ce2ba456576fdad60d54d0a0e
Jan Rudolf <jan.rudolf@plus4u.net>
2022-03-11 12:19:50 +0100

    Day 21 - ha found the error. Had to fix the "areAllergensAssigned", it was obviously wrong. Also optimized it by 50%, still runs at ~10 seconds though.

--- day21/solution.js
+++ day21/solution.js
@@ -2,7 +2,7 @@ const foods = require("./input");
 
 // we setup some globals here, to reuse them in solution of part two
 const ALLERGENS_MAP = groupValues("allergens");
-const LAST_ALERGEN = Object.keys(ALLERGENS_MAP).pop();
+const ALERGENS_LENGTH = Object.keys(ALLERGENS_MAP).length;
 
 const WORDS_TO_ALLRG_MAP = groupAllergens();
 
@@ -33,7 +33,7 @@ function groupAllergens() {
 }
 
 function areAllergensAssigned(wordSetup) {
-  return wordSetup[LAST_ALERGEN];
+  return wordSetup.size === ALERGENS_LENGTH;
 }
 
 function canAssignAllergen(word, allrg) {
@@ -47,30 +47,30 @@ function canAssignAllergen(word, allrg) {
 function findAllrgCombos(allrgSetup, wordSetup, invalidWords) {
   for (let word of Object.keys(WORDS_TO_ALLRG_MAP)) {
     // skip those that are used and those that cannot be already used
-    if (allrgSetup[word] || invalidWords.has(word)) continue;
+    if (allrgSetup.has(word) || invalidWords.has(word)) continue;
 
     for (let allrg of WORDS_TO_ALLRG_MAP[word]) {
       // we skip those allergens that are already assigned to some word
-      if (wordSetup[allrg]) continue;
+      if (wordSetup.has(allrg)) continue;
 
       // we have unused word and unused allergen, now we have to back-check if this current word+allergen
       // is present on all lines, where the allergen should be found
       if (canAssignAllergen(word, allrg)) {
-        allrgSetup[word] = allrg;
-        wordSetup[allrg] = word;
-
-        findAllrgCombos(allrgSetup, wordSetup, invalidWords);
+        allrgSetup.add(word);
+        wordSetup.add(allrg);
 
         if (areAllergensAssigned(wordSetup)) return true;
+
+        findAllrgCombos(allrgSetup, wordSetup, invalidWords);
       }
     }
   }
 
   if (!areAllergensAssigned(wordSetup)) {
-    let lastAddedWord = Object.keys(allrgSetup).pop();
-    let lastAllergen = allrgSetup[lastAddedWord];
-    delete allrgSetup[lastAddedWord];
-    delete wordSetup[lastAllergen];
+    let lastWord = Array.from(allrgSetup).pop();
+    let lastAllergen = Array.from(wordSetup).pop();
+    allrgSetup.delete(lastWord);
+    wordSetup.delete(lastAllergen);
     return false;
   }
 
@@ -90,12 +90,8 @@ function solvePartOne() {
       if (!canAssignAllergen(word, allrg, ALLERGENS_MAP)) return false;
 
       // we need a two-way map, for easy reading of both words and allergens
-      let allrgSetup = {
-        [word]: allrg
-      };
-      let wordSetup = {
-        [allrg]: word
-      };
+      let allrgSetup = new Set([word]);
+      let wordSetup = new Set([allrg]);
       return findAllrgCombos(allrgSetup, wordSetup, invalidWords);
     });
 
@@ -111,23 +107,26 @@ function solvePartOne() {
   return invalidWords;
 }
 
+console.time("partOne");
 let invalidWords = solvePartOne();
+console.timeEnd("partOne");
 
 // part two
 function solvePartTwo(invalidWords) {
   // should be kinda easy and fast - just run the algorithm again, this time with
   // complete invalidWords set, it should go almost directly to only possible combination,
   // because there will be only exactly that many valid words left as there are allergens
-  let allrgSetup = {};
-  let wordSetup = {};
+  let allrgSetup = new Set();
+  let wordSetup = new Set();
   findAllrgCombos(allrgSetup, wordSetup, invalidWords);
 
-  console.log("-> wordSetup", wordSetup);
-  let partTwoResult = Object.keys(wordSetup).sort().map(allrg => wordSetup[allrg]).join(",");
+  let wordArr = Array.from(allrgSetup);
+  let partTwoResult = Array.from(wordSetup)
+    .map((allrg, i) => [allrg, i])
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .map(wordWithI => wordArr[wordWithI[1]])
+    .join(",");
   console.log("-> partTwoResult", partTwoResult);
-
-  // this is not the result! wtf how is it possible
-  // tsqpn,nrl,zqzmzl,tfqsb,xhnk,cltx
 }
 
 solvePartTwo(invalidWords);
